cbc9e20844ae554921d16e3683b318cb90a90450
Deekshigowda08 <149522963+Deekshigowda08@users.noreply.github.com>
2024-02-10 23:08:26 +0530

    go

--- script.js
+++ script.js
@@ -5,19 +5,83 @@ function expandBox() {
     const box = document.getElementById('resizable-box');
 
     if (isExpanded) {
-        // Restore the original size and remove added text
+        
         box.style.width = '60%';
         box.style.height = '70px';
-        box.innerHTML = 'What is Team-7?';
+        box.innerHTML = 'I do not have a team?';
         isExpanded = false;
     } else {
-        // Increase the size of the box
+        
         box.style.width = "80%";
         box.style.height = "30vh";
 
-        // Add text to the box
+       
         const newText = document.createElement('p');
-        newText.textContent ='hi deekshi';
+        newText.textContent =`Don't worry we have a solution for you just go to the chat box and ask for the team members it will give you list and contact the teammates`;
+        box.appendChild(newText);
+
+        isExpanded = true;
+    }
+}
+function expandBox1() {
+    const box = document.getElementById('resizable-box1');
+
+    if (isExpanded) {
+       
+        box.style.width = '60%';
+        box.style.height = '70px';
+        box.innerHTML = 'How to register to these events?';
+        isExpanded = false;
+    } else {
+        
+        box.style.width = "80%";
+        box.style.height = "30vh";
+
+       
+        const newText = document.createElement('p');
+        newText.textContent =`Don't worry we have a solution for you just go to the chat box and ask for the team members it will give you list and contact the teammates`;
+        box.appendChild(newText);
+
+        isExpanded = true;
+    }
+}
+function expandBox2() {
+    const box = document.getElementById('resizable-box2');
+
+    if (isExpanded) {
+       
+        box.style.width = '60%';
+        box.style.height = '70px';
+        box.innerHTML = 'I do not have a team?';
+        isExpanded = false;
+    } else {
+        
+        box.style.width = "80%";
+        box.style.height = "30vh";
+
+        
+        const newText = document.createElement('p');
+        newText.textContent =`Don't worry we have a solution for you just go to the chat box and ask for the team members it will give you list and contact the teammates`;
+        box.appendChild(newText);
+
+        isExpanded = true;
+    }
+}
+function expandBox3() {
+    const box = document.getElementById('resizable-box3');
+
+    if (isExpanded) {
+        
+        box.style.width = '60%';
+        box.style.height = '70px';
+        box.innerHTML = 'I do not have a team?';
+        isExpanded = false;
+    } else {
+        
+        box.style.width = "80%";
+        box.style.height = "30vh"
+        const newText = document.createElement('p');
+        newText.textContent =`Don't worry we have a solution for you just go to the chat box and ask for the team members it will give you list and contact the teammates`;
         box.appendChild(newText);
 
         isExpanded = true;
